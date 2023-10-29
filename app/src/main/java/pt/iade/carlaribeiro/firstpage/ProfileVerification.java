acf865501d93d5c3c9169786d2bbd9e7ae7f21e0
Carla Ribeiro <carlaribeiro@Carlas-MBP.home>
2023-10-29 22:20:41 +0000

    Profile verification and return button

---
+++ app/src/main/java/pt/iade/carlaribeiro/firstpage/ProfileVerification.java
@@ -0,0 +1,14 @@
+package pt.iade.carlaribeiro.firstpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileVerification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_verification);
+    }
+}

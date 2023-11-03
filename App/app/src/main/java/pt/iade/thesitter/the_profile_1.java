270cdf740c6168021cb07741246d3c0767a424ce
LAPTOP-QFELQHAM\tamar <tamaramonteiro2014@icloud.com>
2023-11-03 01:34:44 +0000

    messages_page

---
+++ App/app/src/main/java/pt/iade/thesitter/the_profile_1.java
@@ -0,0 +1,14 @@
+package pt.iade.thesitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class the_profile_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_the_profile1);
+    }
+}

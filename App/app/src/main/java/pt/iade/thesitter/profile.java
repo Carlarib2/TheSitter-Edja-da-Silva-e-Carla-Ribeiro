66c40c91e67785951df51a5f618970ec6ed36baa
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-09 21:05:56 +0000

    Java code added to save button

--- App/app/src/main/java/pt/iade/thesitter/profile.java
+++ App/app/src/main/java/pt/iade/thesitter/profile.java
@@ -2,7 +2,9 @@ package pt.iade.thesitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class profile extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void startSave(View view){
+        Intent intent = new Intent(this, profile_verification.class);
+        startActivity(intent);
     }
 }

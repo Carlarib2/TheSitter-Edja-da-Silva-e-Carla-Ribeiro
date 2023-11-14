c401eb126c3761c9d847460570aaac70f123a3aa
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-14 18:00:05 +0000

    Code changes for request button

--- App/app/src/main/java/pt/iade/thesitter/the_profile_1.java
+++ App/app/src/main/java/pt/iade/thesitter/the_profile_1.java
@@ -2,7 +2,9 @@ package pt.iade.thesitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class the_profile_1 extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class the_profile_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_the_profile1);
+    }
+
+    public void startRequests(View view){
+        Intent intent = new Intent(this, the_profile_2.class);
+        startActivity(intent);
     }
 }

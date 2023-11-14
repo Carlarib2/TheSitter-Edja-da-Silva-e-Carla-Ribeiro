127951bd0801f8a1b00590371cab998ef4e641c1
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-14 17:40:43 +0000

    Camere icon for photo verification added, and onClick attribut set up

--- App/app/src/main/java/pt/iade/thesitter/profile_verification.java
+++ App/app/src/main/java/pt/iade/thesitter/profile_verification.java
@@ -2,7 +2,9 @@ package pt.iade.thesitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class profile_verification extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class profile_verification extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_verification);
+    }
+
+    public void startCamera(View view){
+        Intent intent = new Intent(this, camera_verification.class);
+        startActivity(intent);
     }
 }

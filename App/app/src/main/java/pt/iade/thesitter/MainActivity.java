da3755aa6f2538b517e65356f19613a9008376d1
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-14 17:28:15 +0000

    Code change test

--- App/app/src/main/java/pt/iade/thesitter/MainActivity.java
+++ App/app/src/main/java/pt/iade/thesitter/MainActivity.java
@@ -13,14 +13,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Handler handler = new Handler();
-
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(MainActivity.this, second.class);
-                startActivity(intent);
-            }
-        }, 3000);
     }
 }
--- App/app/src/main/java/pt/iade/thesitter/parent_client_choice.java
+++ App/app/src/main/java/pt/iade/thesitter/parent_client_choice.java
@@ -2,7 +2,9 @@ package pt.iade.thesitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class parent_client_choice extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class parent_client_choice extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_client_choice);
+
+        Handler handler = new Handler();
+
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(parent_client_choice.this, second.class);
+                startActivity(intent);
+            }
+        }, 3000);
     }
 }

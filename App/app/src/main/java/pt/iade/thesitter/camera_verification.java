a48cab7c643082a19d25febe93ce4e1fe2464f2b
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-14 17:18:56 +0000

    Two new activities were created

---
+++ App/app/src/main/java/pt/iade/thesitter/camera_verification.java
@@ -0,0 +1,14 @@
+package pt.iade.thesitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CameraVerificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera_verification);
+    }
+}
---
+++ App/app/src/main/java/pt/iade/thesitter/parent_client_choice.java
@@ -0,0 +1,14 @@
+package pt.iade.thesitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class parent_client_choice extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_client_choice);
+    }
+}

0795be7a6a0180615bd41a9111b48372ae40689d
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-14 17:51:57 +0000

    Document upload activity created

---
+++ App/app/src/main/java/pt/iade/thesitter/document_upload.java
@@ -0,0 +1,14 @@
+package pt.iade.thesitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class document_upload extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_document_upload);
+    }
+}

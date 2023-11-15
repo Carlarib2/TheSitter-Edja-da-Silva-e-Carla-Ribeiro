aed439134cae0feb661739d664b6ec10ce9e53fe
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-15 01:24:37 +0000

    Code updated for buttons

--- App/app/src/main/java/pt/iade/thesitter/profile_settings.java
+++ App/app/src/main/java/pt/iade/thesitter/profile_settings.java
@@ -2,7 +2,9 @@ package pt.iade.thesitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class profile_settings extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class profile_settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_settings);
+    }
+
+    public void startHome2(View view){
+        Intent intent = new Intent(this, messages.class);
+        startActivity(intent);
+    }
+
+    public void startMessages4(View view){
+        Intent intent = new Intent(this, messages.class);
+        startActivity(intent);
+    }
+
+    public void startProfile3(View view){
+        Intent intent = new Intent(this, profile_settings.class);
+        startActivity(intent);
+    }
+
+    public void startRequests3(View view){
+        Intent intent = new Intent(this, the_profile_1.class);
+        startActivity(intent);
     }
 }

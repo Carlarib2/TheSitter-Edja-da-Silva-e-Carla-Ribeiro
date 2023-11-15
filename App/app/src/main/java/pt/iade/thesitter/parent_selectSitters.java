f1085228007fe70f92ea2fef196fa7fb7640a341
LAPTOP-QFELQHAM\tamar <tamaramonteiro2014@icloud.com>
2023-11-15 16:32:46 +0000

    create new activities

---
+++ App/app/src/main/java/pt/iade/thesitter/parent_selectSitters.java
@@ -0,0 +1,14 @@
+package pt.iade.thesitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class parent_selectSitters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_select_sitters);
+    }
+}

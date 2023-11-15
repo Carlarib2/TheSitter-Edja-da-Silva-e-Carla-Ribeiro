cb02557fa42a4175046e47826fedd6e64a002d66
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-15 15:35:52 +0000

    Buttons code changed for testing

--- App/app/src/main/java/pt/iade/thesitter/messages.java
+++ App/app/src/main/java/pt/iade/thesitter/messages.java
@@ -2,7 +2,9 @@ package pt.iade.thesitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class messages extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class messages extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_messages);
+    }
+
+    public void startHome3(View view){
+        Intent intent = new Intent(this, messages.class);
+        startActivity(intent);
+    }
+
+    public void startMessages4(View view){
+        Intent intent = new Intent(this, messages.class);
+        startActivity(intent);
+    }
+
+    public void startProfile3(View view){
+        Intent intent = new Intent(this, profile_settings.class);
+        startActivity(intent);
+    }
+
+    public void startRequests3(View view){
+        Intent intent = new Intent(this, the_profile_1.class);
+        startActivity(intent);
     }
 }

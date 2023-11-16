083fb8ee5567b049fc319f2eb19046152245694e
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-16 17:26:31 +0000

    Parent settings activity created

---
+++ App/app/src/main/java/pt/iade/thesitter/parent_settings.java
@@ -0,0 +1,14 @@
+package pt.iade.thesitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class parent_settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_parent_settings);
+    }
+}

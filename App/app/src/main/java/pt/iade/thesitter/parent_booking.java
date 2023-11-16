5e4c5967ae7d889a253de4f84d2acc09e1e2161a
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-16 17:51:03 +0000

    Can't remember what change was made

--- App/app/src/main/java/pt/iade/thesitter/parent_booking.java
+++ App/app/src/main/java/pt/iade/thesitter/parent_booking.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class parent_bookingRequest extends AppCompatActivity {
+public class parent_booking extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_parent_booking_request);
+        setContentView(R.layout.activity_parent_booking);
     }
 }

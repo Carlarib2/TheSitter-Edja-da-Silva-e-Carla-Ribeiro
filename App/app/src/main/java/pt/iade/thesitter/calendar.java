4c71ff521be504b23bd05c1a19f3bba2baaafe57
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-17 14:07:27 +0000

    Files renamed.

--- App/app/src/main/java/pt/iade/thesitter/calendar.java
+++ App/app/src/main/java/pt/iade/thesitter/calendar.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class parent_calendar extends AppCompatActivity {
+public class calendar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_parent_calendar);
+        setContentView(R.layout.calendar);
     }
 }

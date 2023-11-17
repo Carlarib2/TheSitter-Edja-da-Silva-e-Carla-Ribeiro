1a926bbc965cf093977a53ffa69ddedb62797e7e
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-17 14:21:30 +0000

    My rate and my eperience activities created.

---
+++ App/app/src/main/java/pt/iade/thesitter/my_experience.java
@@ -0,0 +1,14 @@
+package pt.iade.thesitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class my_experience extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_experience);
+    }
+}

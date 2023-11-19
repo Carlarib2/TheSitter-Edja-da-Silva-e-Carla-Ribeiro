8ad0e317ea08f6b6761d275e9334f947ed8f3902
LAPTOP-QFELQHAM\tamar <tamaramonteiro2014@icloud.com>
2023-11-19 19:51:24 +0000

    new

--- App/app/src/main/java/pt/iade/thesitter/MainActivity.java
+++ App/app/src/main/java/pt/iade/thesitter/MainActivity.java
@@ -1,10 +1,15 @@
 package pt.iade.thesitter;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.animation.RectEvaluator;
 import android.content.Intent;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.ImageView;
+import android.animation.ObjectAnimator;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Referência à ImageView no layout pelo ID
+        final ImageView myImageView = findViewById(R.id.logo_image);
+
         Handler handler = new Handler();
 
         handler.postDelayed(new Runnable() {
@@ -20,8 +28,43 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
                 Intent intent = new Intent(MainActivity.this, parent_client_choice.class);
                 startActivity(intent);
+
+                // Adiciona uma animação de transição
+                overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left);
+
+                // Finaliza a atividade atual para que não seja empilhada de volta quando pressionar o botão "Voltar"
+                finish();
             }
         }, 3000);
 
+        // Esconde a ImageView antes de iniciar a animação
+        myImageView.setVisibility(View.INVISIBLE);
+
+        // Adiciona uma animação à ImageView após um pequeno atraso
+        myImageView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Verifica se a largura e altura da ImageView são diferentes de zero
+                if (myImageView.getWidth() != 0 && myImageView.getHeight() != 0) {
+                    // Torna a ImageView visível antes de começar a animação
+                    myImageView.setVisibility(View.VISIBLE);
+
+                    // Adiciona uma animação à ImageView
+                    Rect originalClipBounds = new Rect(0, 0, myImageView.getWidth(), myImageView.getHeight());
+                    myImageView.setClipBounds(originalClipBounds);
+
+                    ObjectAnimator clipAnimator = ObjectAnimator.ofObject(
+                            myImageView,
+                            "clipBounds",
+                            new RectEvaluator(),
+                            new Rect(0, 0, 0, 0),  // Rect inicial (sem recorte)
+                            originalClipBounds   // Rect final (toda a imagem)
+                    );
+
+                    clipAnimator.setDuration(1000);  // Define a duração desejada em milissegundos
+                    clipAnimator.start();
+                }
+            }
+        }, 500); // Aguarde 500 milissegundos antes de iniciar a animação
     }
 }

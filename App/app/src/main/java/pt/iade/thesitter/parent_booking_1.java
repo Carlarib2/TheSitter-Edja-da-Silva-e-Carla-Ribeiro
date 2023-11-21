b2a1558a20143f70018f53a99458846e2ded5d8c
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-21 19:49:27 +0000

    Constraint layout added and onClick setup

--- App/app/src/main/java/pt/iade/thesitter/parent_booking_1.java
+++ App/app/src/main/java/pt/iade/thesitter/parent_booking_1.java
@@ -2,7 +2,9 @@ package pt.iade.thesitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class parent_booking_1 extends AppCompatActivity {
 
@@ -11,4 +13,10 @@ public class parent_booking_1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_booking_1);
     }
+
+    public void startDate(View view){
+        Intent intent = new Intent(this, calendar.class);
+        startActivity(intent);
+    }
+
 }

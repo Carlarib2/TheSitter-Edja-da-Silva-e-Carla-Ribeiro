e92b386353c3550dc4ba8eda7f25413f97fc21e5
LAPTOP-QFELQHAM\tamar <tamaramonteiro2014@icloud.com>
2023-11-22 20:41:21 +0000

    adicionar as definiçoes do calendário

--- App/app/src/main/java/pt/iade/thesitter/calendar.java
+++ App/app/src/main/java/pt/iade/thesitter/calendar.java
@@ -1,14 +1,34 @@
 package pt.iade.thesitter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 import android.os.Bundle;
 
 public class calendar extends AppCompatActivity {
-
+    List<String> datasSelecionadas = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar);
+
+
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                String selectedDate = dayOfMonth + "/" + (month + 1) + "/" + year;
+                Toast.makeText(calendar.this, "Data selecionada: " + selectedDate, Toast.LENGTH_SHORT).show();
+
+                datasSelecionadas.add(selectedDate);
+            }
+        });
     }
 }

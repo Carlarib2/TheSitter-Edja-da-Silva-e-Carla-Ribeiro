4c9703fdf06d41da8ecf92fd68b87ef7a8a387fa
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-23 20:44:11 +0000

    Parent login testint

--- App/app/src/main/java/pt/iade/thesitter/parent_login.java
+++ App/app/src/main/java/pt/iade/thesitter/parent_login.java
@@ -2,7 +2,9 @@ package pt.iade.thesitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class parent_login extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class parent_login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parent_login);
+    }
+
+    public void startRegister4(View view){
+        Intent intent = new Intent(this, register.class);
+        startActivity(intent);
+    }
+
+    public void startLogin3(View view){
+        Intent intent = new Intent(this, parent_home.class);
+        startActivity(intent);
     }
 }

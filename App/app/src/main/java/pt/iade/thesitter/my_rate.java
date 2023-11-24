ab03f31ce81cb9cea266d648f5d27b646448b35f
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-24 15:46:48 +0000

    Buttons of respective files were setup to function

--- App/app/src/main/java/pt/iade/thesitter/my_rate.java
+++ App/app/src/main/java/pt/iade/thesitter/my_rate.java
@@ -13,4 +13,24 @@ public class my_rate extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_rate);
     }
+
+    public void startHome6(View view){
+        Intent intent = new Intent(this, the_profile_1.class);
+        startActivity(intent);
+    }
+
+    public void startRequests6(View view){
+        Intent intent = new Intent(this, requests_view.class);
+        startActivity(intent);
+    }
+
+    public void startMessages7(View view){
+        Intent intent = new Intent(this, messages.class);
+        startActivity(intent);
+    }
+
+    public void startProfile5(View view){
+        Intent intent = new Intent(this, profile_settings.class);
+        startActivity(intent);
+    }
 }

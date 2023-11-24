d5ec3030881402b1b8f69a4a85276447c9c64284
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-24 14:30:20 +0000

    Coding test is being done

--- App/app/src/main/java/pt/iade/thesitter/my_rate.java
+++ App/app/src/main/java/pt/iade/thesitter/my_rate.java
@@ -13,24 +13,4 @@ public class my_rate extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_rate);
     }
-
-    public void startRegister3(View view){
-        Intent intent = new Intent(this, register.class);
-        startActivity(intent);
-    }
-
-    public void startLogin2(View view){
-        Intent intent = new Intent(this, the_profile_1.class);
-        startActivity(intent);
-    }
-
-    public void startRegister3(View view){
-        Intent intent = new Intent(this, register.class);
-        startActivity(intent);
-    }
-
-    public void startLogin2(View view){
-        Intent intent = new Intent(this, the_profile_1.class);
-        startActivity(intent);
-    }
 }

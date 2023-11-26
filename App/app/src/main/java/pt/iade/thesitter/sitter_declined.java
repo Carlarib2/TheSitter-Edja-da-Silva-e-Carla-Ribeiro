78cd464731841e7ed79f7284248105802983c1d7
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-26 19:32:49 +0000

    Updated

--- App/app/src/main/java/pt/iade/thesitter/sitter_declined.java
+++ App/app/src/main/java/pt/iade/thesitter/sitter_declined.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class parent_declined extends AppCompatActivity {
+public class sitter_declined extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_epk_parent_declined);
+        setContentView(R.layout.activity_esg_declined);
     }
 }

486d3f6c84dc42fc49a991f050e61720f0474170
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-26 19:35:22 +0000

    Buttons

--- App/app/src/main/java/pt/iade/thesitter/sitter_declined.java
+++ App/app/src/main/java/pt/iade/thesitter/sitter_declined.java
@@ -2,7 +2,9 @@ package pt.iade.thesitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class sitter_declined extends AppCompatActivity {
 
@@ -10,5 +12,25 @@ public class sitter_declined extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_esg_declined);
+    }
+
+    public void startHomeSD(View view){
+        Intent intent = new Intent(this, the_profile_1.class);
+        startActivity(intent);
+    }
+
+    public void startRequestsSD(View view){
+        Intent intent = new Intent(this, requests_view.class);
+        startActivity(intent);
+    }
+
+    public void startMessagesSD(View view){
+        Intent intent = new Intent(this, messages.class);
+        startActivity(intent);
+    }
+
+    public void startProfileSD(View view){
+        Intent intent = new Intent(this, profile_settings.class);
+        startActivity(intent);
     }
 }

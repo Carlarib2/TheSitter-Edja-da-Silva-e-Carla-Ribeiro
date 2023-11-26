2ea69dc77a41d10ed9fed43c6624be917a9e211e
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-11-26 18:09:41 +0000

    New activities

---
+++ App/app/src/main/java/pt/iade/thesitter/parent_accepted.java
@@ -0,0 +1,14 @@
+package pt.iade.thesitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class parent_accepted extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_epj_parent_accepted);
+    }
+}
---
+++ App/app/src/main/java/pt/iade/thesitter/parent_declined.java
@@ -0,0 +1,14 @@
+package pt.iade.thesitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class parent_declined extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_epk_parent_declined);
+    }
+}

53253e66090e0583c6c0db49e4132211a89b09c4
LAPTOP-QFELQHAM\tamar <tamaramonteiro2014@icloud.com>
2023-12-22 01:42:18 +0000

    add webserver file

---
+++ projectMobile_webserver/projectMobile_webserver/src/test/java/pt/iade/Edjasilva/CarlaRibeiro/projectMobile_webserver/ProjectMobileWebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.iade.Edjasilva.CarlaRibeiro.projectMobile_webserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMobileWebserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

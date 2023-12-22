1aa3e1a88c838000434175664ad81c2cc8b1ae7e
LAPTOP-QFELQHAM\tamar <tamaramonteiro2014@icloud.com>
2023-12-22 01:36:32 +0000

    add new file

---
+++ projectoMobile/projectoMobile/src/main/java/pt/iade/EdjaSilva/CarlaRibeiro/projectoMobile/ProjectoMobileWebServerApplication.java
@@ -0,0 +1,13 @@
+package pt.iade.EdjaSilva.CarlaRibeiro.projectoMobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectoMobileWebServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectoMobileWebServerApplication.class, args);
+	}
+
+}

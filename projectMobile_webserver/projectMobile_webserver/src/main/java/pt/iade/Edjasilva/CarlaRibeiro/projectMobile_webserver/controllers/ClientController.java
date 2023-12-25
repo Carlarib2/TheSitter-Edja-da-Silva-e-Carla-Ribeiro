416d01615d3951db5d71436d8d4af7ed186e2b6c
LAPTOP-QFELQHAM\tamar <tamaramonteiro2014@icloud.com>
2023-12-25 17:25:47 +0000

    editando os controllers

--- projectMobile_webserver/projectMobile_webserver/src/main/java/pt/iade/Edjasilva/CarlaRibeiro/projectMobile_webserver/controllers/ClientController.java
+++ projectMobile_webserver/projectMobile_webserver/src/main/java/pt/iade/Edjasilva/CarlaRibeiro/projectMobile_webserver/controllers/ClientController.java
@@ -1,4 +1,9 @@
 package pt.iade.Edjasilva.CarlaRibeiro.projectMobile_webserver.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path= "/api/client")
 public class ClientController {
 }

e5c30f75e33c49fe7939f5b13e7a5cbadc602a00
LAPTOP-QFELQHAM\tamar <tamaramonteiro2014@icloud.com>
2023-12-28 01:47:39 +0000

    getters finish

--- App/app/src/main/java/pt/iade/thesitter/models/Client.java
+++ App/app/src/main/java/pt/iade/thesitter/models/Client.java
@@ -1,5 +1,33 @@
 package pt.iade.thesitter.models;
 
 public class Client {
-    
+
+
+    private int cliId;
+
+    private int cliUserId;
+
+    private int cliStaId;
+
+    public Client(){
+
+    }
+
+    public Client(int cliId, int cliUserId, int cliStaId) {
+        this.cliId = cliId;
+        this.cliUserId = cliUserId;
+        this.cliStaId = cliStaId;
+    }
+
+    public int getCliId() {
+        return cliId;
+    }
+
+    public int getCliUserId() {
+        return cliUserId;
+    }
+
+    public int getCliStaId() {
+        return cliStaId;
+    }
 }

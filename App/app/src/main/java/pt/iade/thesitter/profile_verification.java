55c84248e98b17e33d0fd4d947b8a23b817d86ab
Carla Ribeiro <carlaribeiro@Carlas-MacBook-Pro.local>
2023-12-29 01:36:58 +0000

    Fixed and camera code updated

--- App/app/src/main/java/pt/iade/thesitter/profile_verification.java
+++ App/app/src/main/java/pt/iade/thesitter/profile_verification.java
@@ -1,31 +1,57 @@
 package pt.iade.thesitter;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
+import android.Manifest;
+
 
 public class profile_verification extends AppCompatActivity {
 
     ImageButton btnCam;
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_de_profile_verification);
 
+
         btnCam = (ImageButton) findViewById(R.id.btnCam);
+
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)!= PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 101);
+        }
+
         btnCam.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent();
                 intent.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(intent);
+                startActivityForResult(intent, 101);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult (int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 101) {
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            btnCam.setImageBitmap(bitmap);
+        }
     }
 
     public void startSave2(View view){

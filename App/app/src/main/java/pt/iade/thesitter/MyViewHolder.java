1e47f6ced42a87b4c023700e056eb8395d925072
LAPTOP-QFELQHAM\tamar <tamaramonteiro2014@icloud.com>
2024-01-03 05:02:14 +0000

    fazendo a recycler view

---
+++ App/app/src/main/java/pt/iade/thesitter/MyViewHolder.java
@@ -0,0 +1,24 @@
+package pt.iade.thesitter;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyViewHolder extends RecyclerView.ViewHolder {
+
+    ImageView selectSitters_image;
+    TextView selectSitters_Name,selectSitters_Gender, selectSitters_Mobile;
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+
+        selectSitters_image= itemView.findViewById(R.id.selectSitters_image);
+        selectSitters_Name= itemView.findViewById(R.id.selectSitters_Name);
+        selectSitters_Gender= itemView.findViewById(R.id.selectSitters_Gender);
+        selectSitters_Mobile=itemView.findViewById(R.id.selectSitters_Mobile);
+
+
+    }
+}
--- App/app/src/main/java/pt/iade/thesitter/adapters/SelectSittersAdapter.java
+++ App/app/src/main/java/pt/iade/thesitter/adapters/SelectSittersAdapter.java
@@ -1,7 +1,35 @@
 package pt.iade.thesitter.adapters;
 
+import android.content.ClipData;
+import android.content.Context;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class SelectSittersAdapter extends RecyclerView.Adapter<SelectSittersAdapter.ViewHolder> {
+import java.util.List;
 
+import pt.iade.thesitter.MyViewHolder;
+
+public class SelectSittersAdapter extends RecyclerView.Adapter<MyViewHolder> {
+
+    Context context;
+    List<Item> items;
+
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
